26a00691ce71969b9f1884b246513843c1921ebd
xuan2k <lexuanhuy248@gmail.com>
2023-10-04 09:13:16 +0700

    Trainer template for deep learning training

---
+++ trainer.py
@@ -0,0 +1,332 @@
+import glob
+import json
+import os
+import time
+
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as standard_transforms
+from PIL import Image
+from skimage import io, transform
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms, utils
+from tqdm import tqdm
+
+from utils import *
+
+
+class Trainer:
+    def __init__(self, options):
+        self.opt = options
+        self.log_path = os.path.join(self.opt.log_dir, self.opt.model_name)
+
+        # MODELS
+        # Load network architecture
+        ''' Setting Models, initialization and optimization '''
+        self.models = {}
+        self.parameters_to_train = []
+
+        self.device = torch.device("cpu" if self.opt.no_cuda else "cuda")
+
+        # self.models["encoder"] = networks.ResnetEncoder(
+        #     50, self.opt.weights_init == "pretrained")
+        # self.models["encoder"].to(self.device)
+        # self.parameters_to_train += list(self.models["encoder"].parameters())
+
+        # self.models["depth"] = networks.DepthDecoder(
+        #     self.models["encoder"].num_ch_enc, self.opt.scales)
+        # self.models["depth"].to(self.device)
+        # self.parameters_to_train += list(self.models["depth"].parameters())
+
+        for key, model in self.models:
+            self.models[key] = self.set_device(model)
+
+        # OPTIMIZER
+        self.model_optimizer = optim.Adam(
+            self.parameters_to_train, self.opt.learning_rate)
+        self.model_lr_scheduler = optim.lr_scheduler.StepLR(
+            self.model_optimizer, self.opt.scheduler_step_size, 0.1)
+
+        if self.opt.load_weights_folder is not None:
+            self.load_model()
+
+        print("Training model named:\n  ", self.opt.model_name)
+        print("Models and tensorboard events files are saved to:\n  ",
+              self.opt.log_dir)
+        print("Training is using:\n  ", self.device)
+
+        # DATA
+
+        # LOGGER
+        self.writers = {}
+        for mode in ["train", "val"]:
+            self.writers[mode] = SummaryWriter(
+                os.path.join(self.log_path, mode))
+
+        # LOSSES AND METRICS
+
+        self.save_opts()
+
+    def set_device(self, module):
+        return module.to(self.device)
+
+    def set_train(self):
+        """Convert all models to training mode
+        """
+        for m in self.models.values():
+            m.train()
+
+    def set_eval(self):
+        """Convert all models to testing/evaluation mode
+        """
+        for m in self.models.values():
+            m.eval()
+
+    def train(self):
+        """Run the entire training pipeline
+        """
+        self.epoch = self.opt.starting_epochs
+        self.step = self.opt.starting_step
+        self.start_time = time.time()
+        for self.epoch in range(self.opt.starting_epochs, self.opt.num_epochs):
+            self.run_epoch()
+            if (self.epoch + 1) % self.opt.save_frequency == 0:
+                self.save_model()
+
+    def run_epoch(self):
+        """Run a single epoch of training and validation
+        """
+        self.model_lr_scheduler.step()
+
+        print("Training epoch {}, start with step {}.".format(
+            self.epoch, self.step))
+        self.set_train()
+
+        for batch_idx, inputs in enumerate(self.train_loader):
+
+            before_op_time = time.time()
+
+            outputs, losses = self.process_batch(inputs)
+
+            self.model_optimizer.zero_grad()
+            losses["loss"].backward()
+            self.model_optimizer.step()
+
+            duration = time.time() - before_op_time
+
+            # log less frequently after the first 2000 steps to save time & disk space
+            early_phase = batch_idx % self.opt.log_frequency == 0 and self.step < 2000
+            late_phase = self.step % 2000 == 0
+
+            if early_phase or late_phase:
+                self.log_time(batch_idx, duration, losses["loss"].cpu().data)
+
+                if "depth_gt" in inputs:
+                    self.compute_depth_losses(inputs, outputs, losses)
+
+                self.log("train", inputs, outputs, losses)
+                self.val()
+
+            self.step += 1
+
+    def process_batch(self, inputs):
+        """Pass a minibatch through the network and generate images and losses
+        """
+        for key, ipt in inputs.items():
+            inputs[key] = ipt.to(self.device)
+
+        if self.opt.pose_model_type == "shared":
+            # If we are using a shared encoder for both depth and pose (as advocated
+            # in monodepthv1), then all images are fed separately through the depth encoder.
+            all_color_aug = torch.cat(
+                [inputs[("color_aug", i, 0)] for i in self.opt.frame_ids])
+            all_features = self.models["encoder"](all_color_aug)
+            all_features = [torch.split(f, self.opt.batch_size)
+                            for f in all_features]
+
+            features = {}
+            for i, k in enumerate(self.opt.frame_ids):
+                features[k] = [f[i] for f in all_features]
+
+            outputs = self.models["depth"](features[0])
+        else:
+            # Otherwise, we only feed the image with frame_id 0 through the depth encoder
+            features = self.models["encoder"](inputs["color_aug", 0, 0])
+            outputs = self.models["depth"](features)
+
+        if self.opt.predictive_mask:
+            outputs["predictive_mask"] = self.models["predictive_mask"](
+                features)
+
+        if self.use_pose_net:
+            outputs.update(self.predict_poses(inputs, features))
+
+        self.generate_images_pred(inputs, outputs)
+        losses = self.compute_losses(inputs, outputs)
+
+        return outputs, losses
+
+    def val(self):
+        """Validate the model on a single minibatch
+        """
+        self.set_eval()
+        try:
+            inputs = self.val_iter.next()
+        except StopIteration:
+            self.val_iter = iter(self.val_loader)
+            inputs = self.val_iter.next()
+
+        with torch.no_grad():
+            outputs, losses = self.process_batch(inputs)
+
+            if "depth_gt" in inputs:
+                self.compute_depth_losses(inputs, outputs, losses)
+
+            self.log("val", inputs, outputs, losses)
+            del inputs, outputs, losses
+
+        self.set_train()
+
+    def generate_images_pred(self, inputs, outputs):
+        """Generate the warped (reprojected) color images for a minibatch.
+        Generated images are saved into the `outputs` dictionary.
+        """
+        pass
+
+    def compute_losses(self, inputs, outputs):
+        """Compute the reprojection and smoothness losses for a minibatch
+        """
+        losses = {}
+        total_loss = 0
+
+        losses["loss"] = total_loss
+        return losses
+
+    def log_time(self, batch_idx, duration, loss):
+        """Print a logging statement to the terminal
+        """
+        samples_per_sec = self.opt.batch_size / duration
+        time_sofar = time.time() - self.start_time
+        training_time_left = (
+            self.num_total_steps / self.step - 1.0) * time_sofar if self.step > 0 else 0
+        print_string = "epoch {:>3} | batch {:>6} | examples/s: {:5.1f}" + \
+            " | loss: {:.5f} | time elapsed: {} | time left: {}"
+        print(print_string.format(self.epoch, batch_idx, samples_per_sec, loss,
+                                  sec_to_hm_str(time_sofar), sec_to_hm_str(training_time_left)))
+
+    def log(self, mode, inputs, outputs, losses):
+        """Write an event to the tensorboard events file
+        """
+        writer = self.writers[mode]
+        for l, v in losses.items():
+            writer.add_scalar("{}".format(l), v, self.step)
+
+        for j in range(min(4, self.opt.batch_size)):  # write a maxmimum of four images
+            for s in self.opt.scales:
+                for frame_id in self.opt.frame_ids:
+                    writer.add_image(
+                        "color_{}_{}/{}".format(frame_id, s, j),
+                        inputs[("color", frame_id, s)][j].data, self.step)
+                    if s == 0 and frame_id != 0:
+                        writer.add_image(
+                            "color_pred_{}_{}/{}".format(frame_id, s, j),
+                            outputs[("color", frame_id, s)][j].data, self.step)
+
+                writer.add_image(
+                    "disp_{}/{}".format(s, j),
+                    normalize_image(outputs[("disp", s)][j]), self.step)
+
+                if self.opt.predictive_mask:
+                    for f_idx, frame_id in enumerate(self.opt.frame_ids[1:]):
+                        writer.add_image(
+                            "predictive_mask_{}_{}/{}".format(frame_id, s, j),
+                            outputs["predictive_mask"][(
+                                "disp", s)][j, f_idx][None, ...],
+                            self.step)
+
+                elif not self.opt.disable_automasking:
+                    writer.add_image(
+                        "automask_{}/{}".format(s, j),
+                        outputs["identity_selection/{}".format(s)][j][None, ...], self.step)
+
+    def show_config(self):
+        to_show = self.opt.__dict__.copy()
+        print(to_show)
+
+    def save_opts(self):
+        """Save options to disk so we know what we ran this experiment with
+        """
+        models_dir = os.path.join(self.log_path, "models")
+        if not os.path.exists(models_dir):
+            os.makedirs(models_dir)
+        to_save = self.opt.__dict__.copy()
+
+        with open(os.path.join(models_dir, 'opt.json'), 'w') as f:
+            json.dump(to_save, f, indent=2)
+
+    def save_model(self):
+        """Save model weights to disk
+        """
+        save_folder = os.path.join(
+            self.log_path, "models", "weights_{}".format(self.epoch))
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder)
+
+        for model_name, model in self.models.items():
+            save_path = os.path.join(save_folder, "{}.pth".format(model_name))
+            to_save = model.state_dict()
+            torch.save(to_save, save_path)
+
+        save_path = os.path.join(save_folder, "{}.pth".format("adam"))
+        torch.save(self.model_optimizer.state_dict(), save_path)
+
+    def load_model(self):
+        """Load model(s) from disk
+        """
+        self.opt.load_weights_folder = os.path.expanduser(
+            self.opt.load_weights_folder)
+
+        assert os.path.isdir(self.opt.load_weights_folder), \
+            "Cannot find folder {}".format(self.opt.load_weights_folder)
+        print("loading model from folder {}".format(
+            self.opt.load_weights_folder))
+
+        for n in self.opt.models_to_load:
+            print("Loading {} weights...".format(n))
+            path = os.path.join(
+                self.opt.load_weights_folder, "{}.pth".format(n))
+            model_dict = self.models[n].state_dict()
+            pretrained_dict = torch.load(path)
+            pretrained_dict = {k: v for k,
+                               v in pretrained_dict.items() if k in model_dict}
+            model_dict.update(pretrained_dict)
+            self.models[n].load_state_dict(model_dict)
+
+        # loading adam state
+        optimizer_load_path = os.path.join(
+            self.opt.load_weights_folder, "adam.pth")
+        if os.path.isfile(optimizer_load_path):
+            print("Loading Adam weights")
+            optimizer_dict = torch.load(optimizer_load_path)
+            self.model_optimizer.load_state_dict(optimizer_dict)
+        else:
+            print("Cannot find Adam weights so Adam is randomly initialized")
+
+
+# Test trainer
+if __name__ == "__main__":
+    
+    opt = Options()
+    opt = opt.parse()
+    
+    trainer = Trainer(opt)
+    trainer.show_config()
+    
+    print("ok")
---
+++ utils/option.py
@@ -0,0 +1,145 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import argparse
+
+file_dir = os.path.dirname(__file__)  # the directory that options.py resides in
+
+
+class Options:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Monodepthv2 options")
+
+        # PATHS
+        self.parser.add_argument("--data_path",
+                                 type=str,
+                                 help="path to the training data",
+                                 default=os.path.join(file_dir, "kitti_data"))
+        self.parser.add_argument("--log_dir",
+                                 type=str,
+                                 help="log directory",
+                                 default=os.path.join(os.path.expanduser("~"), "tmp"))
+
+        # TRAINING options
+        self.parser.add_argument("--model_name",
+                                 type=str,
+                                 help="the name of the folder to save the model in",
+                                 default="mdp")
+        self.parser.add_argument("--split",
+                                 type=str,
+                                 help="which training split to use",
+                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "eigen_split", "eigen_test"],
+                                 default="eigen_zhou")
+        self.parser.add_argument("--num_layers",
+                                 type=int,
+                                 help="number of resnet layers",
+                                 default=18,
+                                 choices=[18, 32, 34, 48, 50, 101, 152, 121, 161, 169, 201])
+        self.parser.add_argument("--dataset",
+                                 type=str,
+                                 help="dataset to train on",
+                                 default="kitti",
+                                 choices=["any", "kitti", "kitti_odom", "kitti_depth", "kitti_test"])
+        self.parser.add_argument("--png",
+                                 help="if set, trains from raw KITTI png files (instead of jpgs)",
+                                 action="store_true")
+        self.parser.add_argument("--height",
+                                 type=int,
+                                 help="input image height",
+                                 default=192)
+        self.parser.add_argument("--width",
+                                 type=int,
+                                 help="input image width",
+                                 default=640)
+        self.parser.add_argument("--disparity_smoothness",
+                                 type=float,
+                                 help="disparity smoothness weight",
+                                 default=1e-3)
+        self.parser.add_argument("--scales",
+                                 nargs="+",
+                                 type=int,
+                                 help="scales used in the loss",
+                                 default=[0, 1, 2, 3])
+        self.parser.add_argument("--min_depth",
+                                 type=float,
+                                 help="minimum depth",
+                                 default=0.1)
+        self.parser.add_argument("--max_depth",
+                                 type=float,
+                                 help="maximum depth",
+                                 default=80.0)
+        self.parser.add_argument("--use_stereo",
+                                 help="if set, uses stereo pair for training",
+                                 action="store_true")
+        self.parser.add_argument("--frame_ids",
+                                 nargs="+",
+                                 type=int,
+                                 help="frames to load",
+                                 default=[0, -1, 1])
+
+        # OPTIMIZATION options
+        self.parser.add_argument("--batch_size",
+                                 type=int,
+                                 help="batch size",
+                                 default=12)
+        self.parser.add_argument("--learning_rate",
+                                 type=float,
+                                 help="learning rate",
+                                 default=1e-4)
+        self.parser.add_argument("--num_epochs",
+                                 type=int,
+                                 help="number of epochs",
+                                 default=20)
+        self.parser.add_argument("--starting_epochs",
+                                 type=int,
+                                 help="starting number of epochs",
+                                 default=0)
+        self.parser.add_argument("--scheduler_step_size",
+                                 type=int,
+                                 help="step size of the scheduler",
+                                 default=15)
+        self.parser.add_argument("--starting_step",
+                                 type=int,
+                                 help="step that start to train",
+                                 default=0)                         
+        
+        # SYSTEM options
+        self.parser.add_argument("--no_cuda",
+                                 help="if set disables CUDA",
+                                 action="store_true")
+        self.parser.add_argument("--num_workers",
+                                 type=int,
+                                 help="number of dataloader workers",
+                                 default=12)
+
+        # LOADING options
+        self.parser.add_argument("--load_weights_folder",
+                                 type=str,
+                                 help="name of model to load")
+        self.parser.add_argument("--models_to_load",
+                                 nargs="+",
+                                 type=str,
+                                 help="models to load",
+                                 default=["encoder", "depth", "pose_encoder", "pose"])
+
+        # LOGGING options
+        self.parser.add_argument("--log_frequency",
+                                 type=int,
+                                 help="number of batches between each tensorboard log",
+                                 default=250)
+        self.parser.add_argument("--save_frequency",
+                                 type=int,
+                                 help="number of epochs between each save",
+                                 default=1)
+
+        
+
+    def parse(self):
+        self.options = self.parser.parse_args()
+        return self.options
